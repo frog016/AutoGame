c2451f72e325245f8af3e0e1172f804e9b433632
frog016 <mixalyov.nikita@mail.ru>
2023-07-14 10:35:54 +0500

    Car
    Added new car.

---
+++ Assets/Scripts/Driving/PolygonFix.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PolygonFix : MonoBehaviour
+{
+    [SerializeField] private float _scale;
+    [SerializeField] private PolygonCollider2D _collider;
+
+    [ContextMenu(nameof(Fix))]
+    public void Fix()
+    {
+        var points = new Vector2[_collider.points.Length];
+        for (var i = 0; i < points.Length; i++)
+            points[i] = _collider.points[i] * _scale;
+
+        _collider.points = points;
+    }
+}
