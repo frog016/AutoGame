5b3633fbb0afc4ba41e80a9e00e24c28a2a8c555
frog016 <mixalyov.nikita@mail.ru>
2023-07-27 09:44:26 +0500

    Coin spending

--- Assets/Scripts/Upgrade/UpgradableParameterConfig.cs
+++ Assets/Scripts/Upgrade/UpgradableParameterConfig.cs
@@ -9,7 +9,8 @@ namespace Upgrade
         [field: SerializeField] public float Factor { get; private set; }
         [field: SerializeField] public int MaxUpgradeLevel { get; private set; }
         [field: SerializeField] public int CurrentLevel { get; private set; }
-
+        [field: SerializeField] public int Cost { get; private set; } = 1;
+        
         [field: SerializeField] public string Name { get; private set; }
 
         public void Upgrade()
--- Assets/Scripts/Upgrade/UpgradeSystem.cs
+++ Assets/Scripts/Upgrade/UpgradeSystem.cs
@@ -1,11 +1,14 @@
 using Driving;
 using System;
+using UnityEngine;
 
 namespace Upgrade
 {
     public class UpgradeSystem
     {
         private readonly CarStatsConfig _carStats;
+
+        private static readonly string CostKey = "CoinsAmount";
 
         public UpgradeSystem(CarStatsConfig config)
         {
@@ -14,16 +17,24 @@ namespace Upgrade
 
         public void Upgrade(UpgradableParameterConfig config, Action<CarStatsConfig, float> upgradeAction)
         {
-            if (IsLevelMax(config))
+            if (IsLevelMax(config) || IsEnough(config.Cost) == false)
                 return;
 
             upgradeAction.Invoke(_carStats, config.Factor);
+            var value = PlayerPrefs.GetInt(CostKey) - config.Cost;
+            PlayerPrefs.SetInt(CostKey, value);
         }
 
         private static bool IsLevelMax(UpgradableParameterConfig config)
         {
             var nextLevel = config.CurrentLevel + 1;
             return nextLevel > config.MaxUpgradeLevel;
+        }
+
+        private static bool IsEnough(int cost)
+        {
+            var value = PlayerPrefs.GetInt(CostKey);
+            return cost <= value;
         }
     }
 }
