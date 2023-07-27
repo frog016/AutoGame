8b5a4745b0a05046c30e1381b705af2053f3f44d
Ilya Sychkov <Sychqh@gmail.com>
2023-07-27 09:50:49 +0500

    Timer

--- Assets/Scripts/UI/Timer.cs
+++ Assets/Scripts/UI/Timer.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Cutsceens;
 
 public class Timer : MonoBehaviour
 {
+	[SerializeField] public float timeRemaining;
+	[SerializeField] private Text timeText;
+	[SerializeField] private CutsceenLoadArguments cutsceenLoader;
+	public bool timeIsRunning = true;
+	
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeIsRunning = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timeIsRunning) 
+		{
+			if (timeRemaining >= 0) 
+			{
+				timeRemaining -= Time.deltaTime;
+				DisplayTime(timeRemaining);
+			}
+			else
+			{
+				cutsceenLoader.LoadCutsceenArgs(CutsceenName.BrokeCar, CutsceenLoadArguments.CutsceenState.End);
+				SceneManager.LoadScene("CutscenePlayer");
+			}
+		}
+		
     }
+	
+	private void DisplayTime(float timeToDisplay)
+	{
+		timeToDisplay += 1;
+		var minutes = Mathf.FloorToInt(timeToDisplay / 60);
+		var seconds = Mathf.FloorToInt(timeToDisplay % 60);
+		timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+	}
 }
